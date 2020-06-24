c798718178a8f9b98b45719742265034290070be
Caterina Gambetti <caterina.gambetti@studio.unibo.iy>
2020-06-24 15:56:21 +0200

    logs page

--- BBManager GIT/assets/signal_manager.py
+++
@@ -1,9 +0,0 @@
-import signal
-
-def signal_handler(signal, frame):
-    print( 'Exiting from server (Ctrl+C pressed)')
-    try:
-      if( server ):
-        server.server_close()
-    finally:
-      sys.exit(0)
